d64d30a64d68e162f62ab0dfc16e6ddc6ec52b6e
Krzysztof Witkowski <krzysiekwit@gmail.com>
2021-04-18 17:46:35 +0200

    init commit + first section and navbar

---
+++ src/components/LandingPage/navBar/Navbar.js
@@ -0,0 +1,29 @@
+import React from "react"
+import { Navbar, NavDropdown, Nav } from "react-bootstrap"
+const navBar = () => {
+  return (
+    <Navbar
+      fixed="top"
+      bg="transparent"
+      expand="sm"
+      className="navbar pt-0 pl-5"
+    >
+      <Navbar.Brand href="#home" style={{ color: "white", fontSize: "30px" }}>
+        Logo
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Link href="#home" style={{ color: "black" }}>
+            Home
+          </Nav.Link>
+          <Nav.Link href="#link" style={{ color: "black" }}>
+            Link
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  )
+}
+
+export default navBar
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,27 @@
-import React from "react"
+import React, { Component } from "react"
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Container } from "react-bootstrap"
+import Navbar from "../components/LandingPage/navBar/Navbar"
+import FirstSection from "../components/LandingPage/FirstSection/FirstSection"
+class landing extends Component {
+  componentDidMount() {}
+  constructor(props) {
+    super(props)
+    this.firstSceneRef = React.createRef()
+  }
+  state = {
+    secondScene: false,
+    thirdScene: false,
+    fourthScene: false,
+  }
 
-export default function Home() {
-  return <div>Hello world!</div>
+  render() {
+    return (
+      <>
+        <Navbar />
+        <FirstSection />
+      </>
+    )
+  }
 }
+export default landing
